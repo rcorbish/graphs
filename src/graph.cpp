187f64d54401b2357567fa158d893028889b2c74
richard@outlook.com <rcorbish@outlook.com>
2022-05-29 15:02:08 -0400

    Move to 3D start

--- src/graph.cpp
+++ src/graph.cpp
@@ -4,132 +4,131 @@
 
 #include <set>
 
-
-
-Graph::Graph( std::string name, int numNodes ) : _name( name ) {
-            adjacencyMatrix.reserve( numNodes ) ;
-            singularValues.reserve( numNodes ) ;    
-            singularVectors.reserve( numNodes*numNodes ) ;
-            for( int i=0 ; i<numNodes ; i++ ) {
-                adjacencyMatrix.emplace_back() ;
-            }
+Graph::Graph(std::string name, int numNodes) : _name(name) {
+    adjacencyMatrix.reserve(numNodes);
+    singularValues.reserve(numNodes);
+    singularVectors.reserve(numNodes * numNodes);
+    for (int i = 0; i < numNodes; i++) {
+        adjacencyMatrix.emplace_back();
+    }
 }
 
 Graph::~Graph() {
-
 }
 
-void Graph::addEdge( int from, int to ) {
-    adjacencyMatrix[from].push_back( to ) ;
+void Graph::addEdge(int from, int to) {
+    adjacencyMatrix[from].push_back(to);
 }
 
-std::vector<int> &Graph::edges( int from ) {
-    return adjacencyMatrix[ from ] ;
+std::vector<int> &Graph::edges(int from) {
+    return adjacencyMatrix[from];
 }
 
 void Graph::calculate() {
-    incidence() ;
-    std::vector<double> lap ;
-    lap.resize( numNodes() * numNodes() ) ;
-    gram( &incidenceMatrix[0], &lap[0], numNodes(), numEdges() ) ;
+    incidence();
+    std::vector<double> lap;
+    lap.resize(numNodes() * numNodes());
+    gram(&incidenceMatrix[0], &lap[0], numNodes(), numEdges());
 
-    int m = numNodes() ;
-    int n = numNodes() ;
-    int k = numNodes() ;
+    int m = numNodes();
+    int n = numNodes();
+    int k = numNodes();
 
-    singularValues.resize( m ) ;
-    singularVectors.resize( m * m ) ;
-    
-    eigs( &singularValues[0], &singularVectors[0] , &lap[0], m ) ;
+    singularValues.resize(m);
+    singularVectors.resize(m * m);
+
+    eigs(&singularValues[0], &singularVectors[0], &lap[0], m);
 }
-
-
 
 void Graph::incidence() {
 
-    int num_edges = 0 ;
-    for( auto node : adjacencyMatrix ) {
-        num_edges += node.size() ;
+    int num_edges = 0;
+    for (auto node : adjacencyMatrix) {
+        num_edges += node.size();
     }
 
-    const size_t num_nodes = adjacencyMatrix.size() ;
+    const size_t num_nodes = adjacencyMatrix.size();
 
-    incidenceMatrix.clear() ;
-    incidenceMatrix.resize( num_nodes*num_edges) ;
-    std::fill(incidenceMatrix.begin(), incidenceMatrix.end(), 0.0 );
+    incidenceMatrix.clear();
+    incidenceMatrix.resize(num_nodes * num_edges);
+    std::fill(incidenceMatrix.begin(), incidenceMatrix.end(), 0.0);
 
-    int edge = 0 ;
-    for( int n=0 ; n<numNodes() ; n++ ) {        
-        for( auto destination : adjacencyMatrix[n] ) {
-            incidenceMatrix[edge*num_nodes+n] = -1.0 ;
-            incidenceMatrix[edge*num_nodes+destination] = 1.0 ;
-            edge++ ;
+    int edge = 0;
+    for (int n = 0; n < numNodes(); n++) {
+        for (auto destination : adjacencyMatrix[n]) {
+            incidenceMatrix[edge * num_nodes + n] = -1.0;
+            incidenceMatrix[edge * num_nodes + destination] = 1.0;
+            edge++;
         }
-    } 
+    }
 }
 
 size_t Graph::numNodes() {
-    return adjacencyMatrix.size() ;
+    return adjacencyMatrix.size();
 }
 
 size_t Graph::numEdges() {
-    size_t num_edges = 0 ;
-    for( auto node : adjacencyMatrix ) {
-        num_edges += node.size() ;
+    size_t num_edges = 0;
+    for (auto node : adjacencyMatrix) {
+        num_edges += node.size();
     }
-    return num_edges ;
+    return num_edges;
 }
 
-std::string Graph::name() { return this->_name ; } 
+std::string Graph::name() { return this->_name; }
 
-std::vector<Point> Graph::getCoords( int a, int b ) {
+std::vector<Point> Graph::getCoords(int a, int b) {
 
-    double *U = &singularVectors[0] ;
+    double *U = &singularVectors[0];
 
-    std::vector<Point> points ;
-    double *x = U + a*numNodes() ;
-    double *y = U + b*numNodes() ;
+    std::vector<Point> points;
+    double *x = U + a * numNodes();
+    double *y = U + b * numNodes();
 
-    for( int i=0 ; i<numNodes() ; i++ ) {
-        points.emplace_back( x[i], y[i] ) ;
+    for (int i = 0; i < numNodes(); i++) {
+        points.emplace_back(x[i], y[i]);
     }
 
-    return points ;
+    return points;
 }
 
+std::vector<LineSegment> Graph::getLines(std::vector<Point> points) {
 
-std::vector<LineSegment> Graph::getLines( std::vector<Point> points )  {
+    std::vector<LineSegment> lines;
+    lines.reserve(numEdges());
 
-    std::vector<LineSegment> lines ;
-    lines.reserve( numEdges() ) ;
+    std::set<std::pair<int, int>> existingLines;
 
-    std::set<std::pair<int,int>> existingLines ;
-
-    int n = 0 ;
-    for( auto point : points ) {
-        for( auto e : edges(n) ) {
-            int a,b ;
-            if( n>=e ) { a=n ; b=e ; } else { a=e ; b=n ; }
-            std::pair<int,int> thisLine( a , b ) ;
-            if( existingLines.find(thisLine) == existingLines.end() ) {
-                lines.emplace_back( point, points[e] ) ;
-                existingLines.emplace( a,b ) ;
+    int n = 0;
+    for (auto point : points) {
+        for (auto e : edges(n)) {
+            int a, b;
+            if (n >= e) {
+                a = n;
+                b = e;
+            } else {
+                a = e;
+                b = n;
+            }
+            std::pair<int, int> thisLine(a, b);
+            if (existingLines.find(thisLine) == existingLines.end()) {
+                lines.emplace_back(point, points[e]);
+                existingLines.emplace(a, b);
             }
         }
-        n++ ;
+        n++;
     }
-    return lines ;
+    return lines;
 }
 
-
-void Graph::print( std::ostream &os ) {
-    int n = 0 ; 
-    for( auto node : adjacencyMatrix ) {
-        os << n << " " ;
-        n++ ;
-        for( auto tgt : node ) {
-            os << tgt << " " ;
+void Graph::print(std::ostream &os) {
+    int n = 0;
+    for (auto node : adjacencyMatrix) {
+        os << n << " ";
+        n++;
+        for (auto tgt : node) {
+            os << tgt << " ";
         }
-        os << std::endl ;
+        os << std::endl;
     }
 }
